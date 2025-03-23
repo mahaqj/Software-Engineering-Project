0d1d3ad87f5694fc00e4a612a9797dbcad55dbc2
S-Amna-Amir <i221142@nu.edu.pk>
2025-03-23 22:22:42 +0500

    Changed frontend + settings.py + static folder
    Made UI, added CSS, JS and HTML for a functioning and appealing frontend
    Changed debug settings and static directory in settings.py
    Added a static folder, and added images for styling

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ydz5wwgf1hyr8=0)ev%d=&d!h!&$_ahpo#1jtfjf*tdn@m3pqa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 LOGIN_URL = "/warehouse_inventory/login/"  # Change this to match your login URL
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
@@ -117,7 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILESDIRS = [BASE_DIR / 'static']
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -131,8 +133,12 @@ AUTHENTICATION_BACKENDS = [
 AUTH_USER_MODEL = 'warehouse_inventory.User'
 
 #to handle pictures
-# import os
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+import os
+MEDIA_URL = '/media/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'warehouse_inventory', 'static'),
+]
